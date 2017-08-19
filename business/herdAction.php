6c125e7b3a3800c750eee91f89083ba505a1c087
adancalfaro93 <adanca16@gmail.com>
2017-08-19 16:57:28 -0600

    Adan

--- business/herdAction.php
+++ business/herdAction.php
@@ -3,7 +3,6 @@
 
 
 	if (isset($_POST['registerHerd'])) {
-		echo "Escuche el boton";
 // Personal information
 		$socioid = $_POST['socioid'];
 		$socioname = $_POST['socioname'];
@@ -12,12 +11,6 @@
 		$sociophonehome = $_POST['sociophonehome'];
 		$sociophone = $_POST['sociophone'];
 
-		// Activities Information
-
-		$milk = $_POST['$milk'];
-		$breedingMeat = $_POST['$breedingMeat'];
-		$fatteningMeat = $_POST['$fatteningMeat'];
-		$doublePurpose = $_POST['$doublePurpose'];
 
 		////  Quantity Animals
 		$calf = $_POST['calf'];
@@ -28,44 +21,51 @@
 		$bull =$_POST['bull'];
 		$cow =$_POST['cow'];
 
-
+		$activitytype = "";
 
 
  
 		if (strlen($socioid)> 0 && strlen($socioname) > 0  &&  strlen($sociofirst)  &&  strlen($sociolast) && strlen($sociophonehome) > 0  && strlen($sociophone) > 0) {
 
-			if(isset($milk))
-			 {
-			   echo "yeah milk";
-			 }  
 
-
-
-			if (strlen($calf) > 0 || strlen($beal) > 0 ||strlen($steer) > 0 || strlen($heifer) > 0 || strlen($impregnatedHeifer) > 0 || strlen($bull) > 0  || strlen($cow) > 0 ) {
-				/// consultA PARA sabeer si el valor digitado es valido
-				if (is_numeric($calf) &&  $calf >  0 || is_numeric($beal) && $beal > 0 ||
-					is_numeric($steer) && $steer > 0 || is_numeric($heifer) && $heifer > 0 || 
-					is_numeric($impregnatedHeifer) && $impregnatedHeifer > 0 || is_numeric($bull)&& $bull > 0  || is_numeric($cow) && $cow > 0 ) {
-					
-
-					echo "Datos totalmente correctos";
-				}else{
-					header("location: ../view/censoView.php?error=numberFormat");	
-				}
-
-			}else{
-				header("location: ../view/censoView.php?error=emptyField");	
+			if ($_POST['milk']) {
+				$activitytype.="1,";	# code...
+			}
+			if ($_POST['breedingMeat']) {
+				$activitytype.="2,";	# code...	
+			}
+			if ($_POST['fatteningMeat']) {
+				$activitytype.="3,";	# code...	
+			}
+			if ($_POST['doublePurpose']) {
+				$activitytype.="4,";	# code...	
 			}
 
+			
+//			echo "Actividad: ".$activitytype;
 
+			if (strlen($activitytype) != 0) {
+				if (strlen($calf) > 0 || strlen($beal) > 0 ||strlen($steer) > 0 || strlen($heifer) > 0 || strlen($impregnatedHeifer) > 0 || strlen($bull) > 0  || strlen($cow) > 0 ) {
+				/// consultA PARA sabeer si el valor digitado es valido
+					if (is_numeric($calf) &&  $calf >  0 || is_numeric($beal) && $beal > 0 ||
+						is_numeric($steer) && $steer > 0 || is_numeric($heifer) && $heifer > 0 || 
+						is_numeric($impregnatedHeifer) && $impregnatedHeifer > 0 || is_numeric($bull)&& $bull > 0  || is_numeric($cow) && $cow > 0 ) {
+					
+					echo "Datos totalmente correctos";
+
+
+					}else{
+						header("location: ../view/censoView.php?error=numberFormat");	
+					}
+				}else{
+					header("location: ../view/censoView.php?error=emptyField");	
+				}
+			}else{
+				header("location: ../view/censoView.php?error=emptyActivity");	
+			}
 		}else{	
 			header("location: ../view/censoView.php?error=emptyField");
 		}
-
-
 	}
-
-
-
 
 ?>
