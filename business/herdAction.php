0eeaa034c02873e87897de1753036629296fa4d7
adancalfaro93 <adanca16@gmail.com>
2017-08-19 18:21:31 -0600

    AA

--- business/herdAction.php
+++ business/herdAction.php
@@ -1,6 +1,6 @@
 <?php
 	include './herdBusiness.php';
-
+	include './herdActivityBusiness.php';
 
 	if (isset($_POST['registerHerd'])) {
 // Personal information
@@ -20,7 +20,7 @@
 		$impregnatedHeifer = $_POST['impregnatedHeifer'];
 		$bull =$_POST['bull'];
 		$cow =$_POST['cow'];
-
+		//Variable para saber el tipo de actividad...
 		$activitytype = "";
 
 
@@ -28,30 +28,45 @@
 		if (strlen($socioid)> 0 && strlen($socioname) > 0  &&  strlen($sociofirst)  &&  strlen($sociolast) && strlen($sociophonehome) > 0  && strlen($sociophone) > 0) {
 
 
-			if ($_POST['milk']) {
+			if (isset($_POST['milk'])) {
 				$activitytype.="1,";	# code...
 			}
-			if ($_POST['breedingMeat']) {
+			if (isset($_POST['breedingMeat'])) {
 				$activitytype.="2,";	# code...	
 			}
-			if ($_POST['fatteningMeat']) {
+			if (isset($_POST['fatteningMeat'])) {
 				$activitytype.="3,";	# code...	
 			}
-			if ($_POST['doublePurpose']) {
+			if (isset($_POST['doublePurpose'])) {
 				$activitytype.="4,";	# code...	
-			}
-
-			
-//			echo "Actividad: ".$activitytype;
-
+			}		
+			// Valido para saber si selecciono almenos una actividad..
 			if (strlen($activitytype) != 0) {
 				if (strlen($calf) > 0 || strlen($beal) > 0 ||strlen($steer) > 0 || strlen($heifer) > 0 || strlen($impregnatedHeifer) > 0 || strlen($bull) > 0  || strlen($cow) > 0 ) {
 				/// consultA PARA sabeer si el valor digitado es valido
 					if (is_numeric($calf) &&  $calf >  0 || is_numeric($beal) && $beal > 0 ||
 						is_numeric($steer) && $steer > 0 || is_numeric($heifer) && $heifer > 0 || 
 						is_numeric($impregnatedHeifer) && $impregnatedHeifer > 0 || is_numeric($bull)&& $bull > 0  || is_numeric($cow) && $cow > 0 ) {
-					
-					echo "Datos totalmente correctos";
+
+						$herd = new Herd($socioid,$calf ,$beal,$steer,$heifer,$impregnatedHeifer,$bull,$cow);
+					//	$hearBusiness = new herdBusiness();
+
+				//		$result = $hearBusiness->insertTBHerd($herd);
+					/*	if ($result ==1) {
+							echo "Insertado con exitooo";
+						/*	$herdActivityBusiness = new herdActivityBusiness();
+							$result2=$herdActivityBusiness->insertTBHerdActivity($socioid,$activitytype);
+							if ($result2==1) {
+								header("location: ../index.php");		
+							}else{
+								header("location: ../view/censoView.php?error=registerHerdActivity");		
+							}
+						
+						}else{
+							header("location: ../view/censoView.php?error=registerHerd");	
+						}
+					*/
+					echo "Datos totalmente correctos: activitytype: ".$activitytype;
 
 
 					}else{
---
+++ business/herdActivityBusiness.php
@@ -0,0 +1,27 @@
+<?php
+	/**
+	* 
+	*/
+	include '../data/herdActivityData.php';
+
+	
+	class herdActivityBusiness 
+	{
+		private $herdActivityBusiness;
+		
+
+		function herdActivityBusiness()
+		{
+			$this->herdActivityBusiness = new herdActivityData();
+
+		}
+
+		public function insertTBHerdActivity($idperson, $activitytype)
+		{
+			return $this->herdActivityBusiness->insertTBHerdActivity($idperson,$activitytype);
+		}
+
+
+	}
+
+?>
