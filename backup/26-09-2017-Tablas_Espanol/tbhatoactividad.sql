a483c87ff6664b59268a6542afb4c42834d4c7c7
adancalfaro93 <adanca16@gmail.com>
2017-08-26 16:53:42 -0600

    Addna

---
+++ backup/26-09-2017-Tablas_Espanol/tbhatoactividad.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 27, 2017 at 12:44 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbasoturga`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbhatoactividad`
+--
+
+CREATE TABLE `tbhatoactividad` (
+  `hatoactividadid` int(11) NOT NULL,
+  `hatoactividadpersonaid` varchar(30) NOT NULL,
+  `hatoactividadtipo` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbhatoactividad`
+--
+
+INSERT INTO `tbhatoactividad` (`hatoactividadid`, `hatoactividadpersonaid`, `hatoactividadtipo`) VALUES
+(1, '503960363', '1,2,3'),
+(2, '503960368', '1,2,4'),
+(3, '909090', '1,'),
+(4, '1-123-123', '1,4,'),
+(5, '2894', '1,'),
+(6, '122', '1,'),
+(7, '34', '1,'),
+(8, '1212', '1,'),
+(9, '123', '1,3,');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbhatoactividad`
+--
+ALTER TABLE `tbhatoactividad`
+  ADD PRIMARY KEY (`hatoactividadid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbhatoactividad`
+--
+ALTER TABLE `tbhatoactividad`
+  MODIFY `hatoactividadid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
