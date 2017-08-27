1c6f381924714ba05c1278ee259016d5fc2d68c4
2017JosePablo <gordoalfaro@gmail.com>
2017-08-27 00:48:59 -0600

    Arreglo de metodo Actividad

--- data/hatoActividadData.php
+++ data/hatoActividadData.php
@@ -33,12 +33,12 @@ class hatoActividadData extends Data{
         $actividad = array();
 
         $conn = new mysqli($this->data->getServidor(), $this->data->getUsuario(), $this->data->getContrasena(), $this->data->getDbNombre());  
-        $sql = "SELECT * FROM tbactividad";
+        $sql = "SELECT * FROM tbtipoactividad";
         $result = $conn->query($sql);
         if($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
 
-                array_push($actividad,new actividad($row[''],));
+                array_push($actividad,new actividad($row['tipoactividadid'],$row['tipoactividadnombre ']));
             }
         }else{
             echo "0 results";
