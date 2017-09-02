b12a7c475554d8003372cb923fe4c628cd951f8f
adancalfaro93 <adanca16@gmail.com>
2017-09-02 10:58:08 -0600

    Nueva base de datos

---
+++ backup/2 Setiembre 2017/dbasoturga.sql
@@ -0,0 +1,432 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Sep 02, 2017 at 06:56 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbasoturga`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbfinca`
+--
+
+CREATE TABLE `tbfinca` (
+  `fincaid` int(11) NOT NULL,
+  `socioid` int(11) DEFAULT NULL,
+  `fincaarea` int(11) DEFAULT NULL,
+  `fincacantidadbobinos` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbfinca`
+--
+
+INSERT INTO `tbfinca` (`fincaid`, `socioid`, `fincaarea`, `fincacantidadbobinos`) VALUES
+(1, 1, 7, '676');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbfincadireccion`
+--
+
+CREATE TABLE `tbfincadireccion` (
+  `fincaid` int(11) NOT NULL,
+  `fincaprovincia` int(11) NOT NULL,
+  `fincacanton` int(11) DEFAULT NULL,
+  `fincadistrito` int(11) DEFAULT NULL,
+  `fincapueblo` varchar(300) DEFAULT NULL,
+  `fincaexacta` varchar(450) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbfincadireccion`
+--
+
+INSERT INTO `tbfincadireccion` (`fincaid`, `fincaprovincia`, `fincacanton`, `fincadistrito`, `fincapueblo`, `fincaexacta`) VALUES
+(1, 1, 1, 1, 'RIO', 'RIOO');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbfincatipo`
+--
+
+CREATE TABLE `tbfincatipo` (
+  `fincatipoid` int(11) NOT NULL,
+  `fincatiponombre` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbfincatipo`
+--
+
+INSERT INTO `tbfincatipo` (`fincatipoid`, `fincatiponombre`) VALUES
+(1, 'REPASTO'),
+(2, 'ESTAULADO\r\n'),
+(3, 'SEMIESTAULADO');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbhato`
+--
+
+CREATE TABLE `tbhato` (
+  `hatoid` int(11) NOT NULL,
+  `hatopersonaid` varchar(30) NOT NULL,
+  `hatoterneros` int(11) NOT NULL,
+  `hatoterneras` int(11) NOT NULL,
+  `hatonovillos` int(11) NOT NULL,
+  `hatonovillas` int(11) NOT NULL,
+  `hatonovillasprenadas` int(11) NOT NULL,
+  `hatotoros` int(11) NOT NULL,
+  `hatovacas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbhato`
+--
+
+INSERT INTO `tbhato` (`hatoid`, `hatopersonaid`, `hatoterneros`, `hatoterneras`, `hatonovillos`, `hatonovillas`, `hatonovillasprenadas`, `hatotoros`, `hatovacas`) VALUES
+(0, '8', 67, 0, 0, 0, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbhatoactividad`
+--
+
+CREATE TABLE `tbhatoactividad` (
+  `hatoactividadid` int(11) NOT NULL,
+  `hatoactividadpersonaid` varchar(30) NOT NULL,
+  `hatoactividadtipo` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbhatoactividad`
+--
+
+INSERT INTO `tbhatoactividad` (`hatoactividadid`, `hatoactividadpersonaid`, `hatoactividadtipo`) VALUES
+(0, '8', '1'),
+(1, '503960363', '1,2,3'),
+(2, '503960368', '1,2,4'),
+(3, '909090', '1,'),
+(4, '1-123-123', '1,4,'),
+(5, '2894', '1,'),
+(6, '122', '1,'),
+(7, '34', '1,'),
+(8, '1212', '1,'),
+(9, '123', '1,3,'),
+(10, '877', ''),
+(11, 'ADan', ''),
+(12, '7', ''),
+(13, '7', ''),
+(14, '7', ''),
+(15, '7', ''),
+(16, '7', ''),
+(17, '3434', ''),
+(18, '9009', '4');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbjunta`
+--
+
+CREATE TABLE `tbjunta` (
+  `idjunta` varchar(50) NOT NULL,
+  `juntapresidente` varchar(100) NOT NULL,
+  `juntavicepresidente` varchar(100) NOT NULL,
+  `juntatesorero` varchar(100) NOT NULL,
+  `juntasecretario` varchar(100) NOT NULL,
+  `juntavocal1` varchar(100) NOT NULL,
+  `juntavocal2` varchar(100) NOT NULL,
+  `juntavocal3` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbjunta`
+--
+
+INSERT INTO `tbjunta` (`idjunta`, `juntapresidente`, `juntavicepresidente`, `juntatesorero`, `juntasecretario`, `juntavocal1`, `juntavocal2`, `juntavocal3`) VALUES
+('113', 'Da', 'dkAJ', 'dlakJ', 'dlkAJ', 'lkfjs', 'flksJ', 'JHK'),
+('909090', 'u', 'u', 'u', 'u', 'u', 'u', 'u'),
+('dd', 'Jose', 'JUANANANA', 'Juja', 'adam', 'v1', 'v2', 'DSDSD');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbpersona`
+--
+
+CREATE TABLE `tbpersona` (
+  `personaid` int(30) NOT NULL,
+  `personaidentificacion` varchar(30) DEFAULT NULL,
+  `personanombre` varchar(30) DEFAULT NULL,
+  `personaprimerapellido` varchar(30) DEFAULT NULL,
+  `personasegundoapellido` varchar(20) NOT NULL,
+  `personatelefonofjo` varchar(20) NOT NULL,
+  `personacelular` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbraza`
+--
+
+CREATE TABLE `tbraza` (
+  `idraza` int(11) NOT NULL,
+  `razanombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbraza`
+--
+
+INSERT INTO `tbraza` (`idraza`, `razanombre`) VALUES
+(1, 'Jersey'),
+(2, 'Holstein'),
+(3, 'Brahaman'),
+(4, 'Charolais'),
+(5, 'Angus'),
+(6, 'Otros'),
+(8, 'ADAN');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbsocio`
+--
+
+CREATE TABLE `tbsocio` (
+  `socioid` int(11) NOT NULL,
+  `sociocedula` varchar(45) DEFAULT NULL,
+  `socionombre` varchar(45) DEFAULT NULL,
+  `socioprimerapellido` varchar(45) DEFAULT NULL,
+  `sociosegundoapellido` varchar(45) DEFAULT NULL,
+  `sociotelefono` varchar(45) DEFAULT NULL,
+  `sociocorreo` varchar(45) DEFAULT NULL,
+  `tipoactividadid` int(11) DEFAULT NULL,
+  `fincatipoid` int(11) DEFAULT NULL,
+  `sociofechaingreso` date DEFAULT NULL,
+  `estadosociodetalle` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbsocio`
+--
+
+INSERT INTO `tbsocio` (`socioid`, `sociocedula`, `socionombre`, `socioprimerapellido`, `sociosegundoapellido`, `sociotelefono`, `sociocorreo`, `tipoactividadid`, `fincatipoid`, `sociofechaingreso`, `estadosociodetalle`) VALUES
+(1, '1234', 'A', 'V', 'A', 'A', 'ab@ga.com', 1, 1, '2017-08-07', 1),
+(2, '123456', 'Adan', 'CarranzA', 'Alfaro', '33430', 'adanca16@gmail.com', 2, 3, '2017-08-02', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbsociodireccion`
+--
+
+CREATE TABLE `tbsociodireccion` (
+  `socioid` int(11) NOT NULL,
+  `socioprovincia` int(11) DEFAULT NULL,
+  `sociocanton` int(11) DEFAULT NULL,
+  `sociodistrito` int(11) DEFAULT NULL,
+  `sociopueblo` varchar(450) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbsociodireccion`
+--
+
+INSERT INTO `tbsociodireccion` (`socioid`, `socioprovincia`, `sociocanton`, `sociodistrito`, `sociopueblo`) VALUES
+(1, 1, 1, 1, 'Rio');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbsocioestado`
+--
+
+CREATE TABLE `tbsocioestado` (
+  `socioestadoid` int(11) NOT NULL,
+  `socioestadodetalle` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbsocioestado`
+--
+
+INSERT INTO `tbsocioestado` (`socioestadoid`, `socioestadodetalle`) VALUES
+(1, 'moroso'),
+(2, 'Activo'),
+(3, 'inactivo');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbtipoactividad`
+--
+
+CREATE TABLE `tbtipoactividad` (
+  `tipoactividadid` int(11) NOT NULL,
+  `tipoactividadnombre` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbtipoactividad`
+--
+
+INSERT INTO `tbtipoactividad` (`tipoactividadid`, `tipoactividadnombre`) VALUES
+(1, 'Carne'),
+(2, 'Leche'),
+(3, 'Desarrollo'),
+(5, 'SEXIIII');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbfinca`
+--
+ALTER TABLE `tbfinca`
+  ADD PRIMARY KEY (`fincaid`),
+  ADD KEY `socioid` (`socioid`);
+
+--
+-- Indexes for table `tbfincadireccion`
+--
+ALTER TABLE `tbfincadireccion`
+  ADD PRIMARY KEY (`fincaid`);
+
+--
+-- Indexes for table `tbfincatipo`
+--
+ALTER TABLE `tbfincatipo`
+  ADD PRIMARY KEY (`fincatipoid`);
+
+--
+-- Indexes for table `tbhato`
+--
+ALTER TABLE `tbhato`
+  ADD PRIMARY KEY (`hatoid`);
+
+--
+-- Indexes for table `tbhatoactividad`
+--
+ALTER TABLE `tbhatoactividad`
+  ADD PRIMARY KEY (`hatoactividadid`);
+
+--
+-- Indexes for table `tbjunta`
+--
+ALTER TABLE `tbjunta`
+  ADD PRIMARY KEY (`idjunta`);
+
+--
+-- Indexes for table `tbpersona`
+--
+ALTER TABLE `tbpersona`
+  ADD PRIMARY KEY (`personaid`);
+
+--
+-- Indexes for table `tbraza`
+--
+ALTER TABLE `tbraza`
+  ADD PRIMARY KEY (`idraza`);
+
+--
+-- Indexes for table `tbsocio`
+--
+ALTER TABLE `tbsocio`
+  ADD PRIMARY KEY (`socioid`),
+  ADD KEY `tipoactividadid` (`tipoactividadid`),
+  ADD KEY `fincatipoid` (`fincatipoid`),
+  ADD KEY `estadosociodetalle` (`estadosociodetalle`);
+
+--
+-- Indexes for table `tbsociodireccion`
+--
+ALTER TABLE `tbsociodireccion`
+  ADD PRIMARY KEY (`socioid`);
+
+--
+-- Indexes for table `tbsocioestado`
+--
+ALTER TABLE `tbsocioestado`
+  ADD PRIMARY KEY (`socioestadoid`);
+
+--
+-- Indexes for table `tbtipoactividad`
+--
+ALTER TABLE `tbtipoactividad`
+  ADD PRIMARY KEY (`tipoactividadid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbfinca`
+--
+ALTER TABLE `tbfinca`
+  MODIFY `fincaid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `tbfincadireccion`
+--
+ALTER TABLE `tbfincadireccion`
+  MODIFY `fincaid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `tbfincatipo`
+--
+ALTER TABLE `tbfincatipo`
+  MODIFY `fincatipoid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `tbraza`
+--
+ALTER TABLE `tbraza`
+  MODIFY `idraza` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `tbsocio`
+--
+ALTER TABLE `tbsocio`
+  MODIFY `socioid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `tbsociodireccion`
+--
+ALTER TABLE `tbsociodireccion`
+  MODIFY `socioid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `tbsocioestado`
+--
+ALTER TABLE `tbsocioestado`
+  MODIFY `socioestadoid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `tbtipoactividad`
+--
+ALTER TABLE `tbtipoactividad`
+  MODIFY `tipoactividadid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
