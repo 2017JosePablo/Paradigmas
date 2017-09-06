e4ac0c7a2922296a818a6f729376bab9a261f25d
LuisCPC <lz.parrac66@gmail.com>
2017-09-06 00:45:27 -0600

    Parra

--- business/fincaAction.php
+++ business/fincaAction.php
@@ -55,7 +55,7 @@
 
 		require './fincaBusiness.php';
 		require './socioBusiness.php';
-		include '../domain/finca.php';
+		//include '../domain/finca.php';
 		include '../domain/fincaDireccion.php';
 		$socioBusiness= new socioBusiness();
 
