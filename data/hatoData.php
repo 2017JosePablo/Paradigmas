774f005aa0ebba6e9e19b0c638ec800db11e06c0
adancalfaro93 <adanca16@gmail.com>
2017-10-02 18:07:03 -0600

    Adna

--- data/hatoData.php
+++ data/hatoData.php
@@ -112,7 +112,7 @@ public function obtenerSocioHatoModificar($idsocio) {
         if($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
 
-                $hato = ["socioid"=>$row["socioid"],"hatoraza"$row["hatoraza"],"hatoternero"=>$row["hatoternero"],"hatoternera"=>$row["hatoternera"],"hatono"=>$row["hatonovillo"],"hatonovilla"=>$row["hatonovilla"],"hatonovillaprenada"=>$row["hatonovillaprenada"],"hatotoroengorde"=>$row["hatotoroengorde"],"hatotoroservicio"=>$row["hatotoroservicio"],"hatovacacria"=>$row["hatovacacria"],"hatovacaengorde"=>$row["hatovacaengorde"]];
+                $hato = ["socioid"=>$row["socioid"],"hatoraza"=>$row["hatoraza"],"hatoternero"=>$row["hatoternero"],"hatoternera"=>$row["hatoternera"],"hatono"=>$row["hatonovillo"],"hatonovilla"=>$row["hatonovilla"],"hatonovillaprenada"=>$row["hatonovillaprenada"],"hatotoroengorde"=>$row["hatotoroengorde"],"hatotoroservicio"=>$row["hatotoroservicio"],"hatovacacria"=>$row["hatovacacria"],"hatovacaengorde"=>$row["hatovacaengorde"]];
             }
         }
 
