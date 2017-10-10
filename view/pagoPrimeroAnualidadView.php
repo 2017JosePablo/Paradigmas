9ea7ca11f57c507466fbd4a119b60564a1cd0df4
adancalfaro93 <adanca16@gmail.com>
2017-10-10 16:19:39 -0600

    Adna

--- view/pagoPrimeroAnualidadView.php
+++ view/pagoPrimeroAnualidadView.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Primer pago de anualidad</title>
+    <title>Primer pago de Anualidad</title>
   </head>
 
 
@@ -27,45 +27,39 @@
 
   <body>
 
-    <p>Area administrativa de  Renovacion de Socio</p>
+    <p>Area administrativa de primer pago de Anualidad </p>
 
     <form class="" action="../business/registroAnualidadAction.php" method="post">
 
-      echo '<select name= "socioAnualidad" id= "socioAnualidad">';
         <span> Seleccione el Socio a realizar el pago.</span>
+          <?php
 
-        <?php
+          include '../business/socioBusiness.php';
+          $socioBusiness = new socioBusiness();
+          $socios = $socioBusiness->obtenerTodosTBSocio();
 
-        include '../business/socioBusiness.php';
-        $socioBusiness = new socioBusiness();
-        $socios = $socioBusiness->obtenerTodosTBSocio();
+          echo '<select name= "socioAnualidad" id= "socioAnualidad">';
+                echo '<option value = "-1"> Seleccione un socio </option>';
+          foreach ($socios as $current) {
+              echo '<option value = "'.$current->getSocioId().'"> '.$current->getNombre().' '.$current->getPrimerApellido() .' '.$current->getSegundoApellido().'</option>';
+          }
+          echo '</select>';
 
-        echo '<select name= "socioAnualidad" id= "socioAnualidad">';
-              echo '<option value = "-1"> Seleccione un socio </option>';
-        foreach ($socios as $current) {
-            echo '<option value = "'.$current->getSocioId().'"> '.$current->getNombre().' '.$current->getPrimerApellido() .' '.$current->getSegundoApellido().'</option>';
-        }
-        echo '</select>';
-
-        ?>
-        <input type="text" name="socioId" id="socioId" value="" hidden="">
-        <p>Fecha del pago Anterior: </p><input id="fechapagoAnterior" readOnly>
-
-        <br>
-        <br>
-        <?php
-
-        ?>
+          ?>
+          <input type="text" name="socioId" id="socioId"  hidden="">
         
-        <input type="text" name="socioId" id="socioId" value="" hidden="">
-        <br>
-        <br>
+          <br>
+          <br>
+          <?php
+
+          ?>
+
+
           <span> Seleccione la fecha que se realiza el pago.</span>
           <input type="date" name="fechaPago" id="fechaPago">
           <br>
           <br>
-       <input name="renovarAnualidad" value="Guardar Datos" type="submit">
-
+       <input name="primerAnualidad" value="Guardar Datos" type="submit">
        <a href="pagoPrimeroAnualidadView.php"><input type="button" value="Cancelar" ></a>
     </form>
 
