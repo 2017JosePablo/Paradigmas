647db3aae93a2a09d56119ebcd8998c031dc8a31
2017JosePablo <gordoalfaro@gmail.com>
2017-10-10 14:12:59 -0600

    sas

--- business/registroAnualidadBusiness.php
+++ business/registroAnualidadBusiness.php
@@ -1,8 +1,8 @@
 <?php
-	include '../data/PagoAnualidadData.php';
+	include '../data/pagoAnualidadData.php';
 
 	if (isset($_POST['idSocio']) && !empty($_POST['idSocio'])) {
-		
+
 		echo "Adan me la mam***";	
 	}
 	class RegistroAnualidadBusiness {
