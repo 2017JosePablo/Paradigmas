1f3fc4f4d7973148691b75b8b91212841d01d9d6
adancalfaro93 <adanca16@gmail.com>
2017-10-22 15:57:39 -0600

    Ada

--- js/editarSocio.js
+++ js/editarSocio.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('select').on('change', function() {
     if(this.value>0){
       var idsocio =  this.value;
-      //  document.getElementById('socioId').value = idsocio;
+        document.getElementById('socioId').value = idsocio;
     }
   });
 });
