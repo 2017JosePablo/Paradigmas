11bf58cf33380234cecd75a9682d9903a1d63b0a
2017JosePablo <gordoalfaro@gmail.com>
2017-10-22 16:18:31 -0600

    s

--- business/socioAction.php
+++ business/socioAction.php
@@ -60,6 +60,7 @@
 
 			require_once '../domain/socioDireccion.php';
 			require_once '../domain/fincaDireccion.php';
+			include '../domain/socio.php';
 
 			//require_once '../domain/hato.php';
 			//require_once '../domain/finca.php';
@@ -78,68 +79,62 @@
 
 				$resultado0 = $socioBusiness->insertarTBSocio($socio);
 
-				$idSocio=$socioBusiness->getSocioId($cedula);
-				$fincaBusiness= new fincaBusiness();
-				$finca= new Finca('',$idSocio,'','','');
-				$resultado1=$fincaBusiness->insertarFinca($finca);
 
 
+				//si inserta el socio entonces se disparn las otras tablas insert
+				if($resultado0==1){
 
-				$socioDireccion = new socioDireccion('',$provincia,$canton,$distrito,$pueblo);
-				$resultado2 = $socioBusiness-> insertarTBSocioDireccion($socioDireccion);
+					$idSocio=$socioBusiness->getSocioId($cedula);
+					$fincaBusiness= new fincaBusiness();
+					$finca= new Finca('',$idSocio,'','','');
+					$resultado1=$fincaBusiness->insertarFinca($finca);
 
-				$fincaDireccion= new fincaDireccion('','','','','','');
-				$resultado3= $fincaBusiness->insertarTBFincaDireccion($fincaDireccion);
-
-
-				////////////////////////////////SUBIENDO IMAGEN
-				$nombre_img = $_FILES['imagen']['name'];
-				$tipo = $_FILES['imagen']['type'];
-				$tamano = $_FILES['imagen']['size'];
-
-				//Si existe imagen y tiene un tamaño correcto
-				if (($nombre_img == !NULL) && ($_FILES['imagen']['size'] <= 200000)){
-				   //indicamos los formatos que permitimos subir a nuestro servidor
-				   if (($_FILES["imagen"]["type"] == "image/gif")
-				   || ($_FILES["imagen"]["type"] == "image/jpeg")
-				   || ($_FILES["imagen"]["type"] == "image/jpg")
-				   || ($_FILES["imagen"]["type"] == "image/png"))
-				   {
-				      // Ruta donde se guardarán las imágenes que subamos
-				     $directorio = $_SERVER['DOCUMENT_ROOT'].'/paradigmas/uploads/';
-				      // Muevo la imagen desde el directorio temporal a nuestra ruta indicada anteriormente
-				      move_uploaded_file($_FILES['imagen']['tmp_name'],$directorio.$nombre_img);
-				    }else{
-				       //si no cumple con el formato
-				       echo "No se puede subir una imagen con ese formato ";
-				    }
-				}else{
-				   //si existe la variable pero se pasa del tamaño permitido
-				   if($nombre_img == !NULL) echo "La imagen es demasiado grande ";
-				}
-
-				////////////////////////////////SUBIENDO IMAGEN
-
-			//	require './'
+					$socioDireccion = new socioDireccion('',$provincia,$canton,$distrito,$pueblo);
+					$fincaDireccion= new fincaDireccion('','','','','','');
+					$resultado3= $fincaBusiness->insertarTBFincaDireccion($fincaDireccion);
 
 
+					$nombre_img = $_FILES['imagen']['name'];
+					$tipo = $_FILES['imagen']['type'];
+					$tamano = $_FILES['imagen']['size'];
 
-				require './hatoBusiness.php';
-				$hato = new Hato($idSocio,'','','','','','','','','','');
-				$hatoBusiness = new hatoBusiness();
-				$resultado4 = $hatoBusiness->insertarTBHato($hato);
+					//Si existe imagen y tiene un tamaño correcto
+					if (($nombre_img == !NULL) && ($_FILES['imagen']['size'] <= 200000)){
+					   //indicamos los formatos que permitimos subir a nuestro servidor
+					   if (($_FILES["imagen"]["type"] == "image/gif")
+					   || ($_FILES["imagen"]["type"] == "image/jpeg")
+					   || ($_FILES["imagen"]["type"] == "image/jpg")
+					   || ($_FILES["imagen"]["type"] == "image/png"))
+					   {
+					      // Ruta donde se guardarán las imágenes que subamos
+					     $directorio = $_SERVER['DOCUMENT_ROOT'].'/paradigmas/uploads/';
+					      // Muevo la imagen desde el directorio temporal a nuestra ruta indicada anteriormente
+					      move_uploaded_file($_FILES['imagen']['tmp_name'],$directorio.$nombre_img);
+					    }else{
+					       //si no cumple con el formato
+					       echo "No se puede subir una imagen con ese formato ";
+					    }
+					}else{
+					   //si existe la variable pero se pasa del tamaño permitido
+					   if($nombre_img == !NULL) echo "La imagen es demasiado grande ";
+					}
 
-				require './hatoActividadBusiness.php';
-				$hatoActividadBusiness = new hatoActividadBusiness();
-				$resultado5=$hatoActividadBusiness->insertarTBHatoActividad($idSocio,$tipoactividad);
+					require './hatoBusiness.php';
+					$hato = new Hato($idSocio,'','','','','','','','','','');
+					$hatoBusiness = new hatoBusiness();
+					$resultado4 = $hatoBusiness->insertarTBHato($hato);
 
-
+					require './hatoActividadBusiness.php';
+					$hatoActividadBusiness = new hatoActividadBusiness();
+					$resultado5=$hatoActividadBusiness->insertarTBHatoActividad($idSocio,$tipoactividad);
 
 					require './aprovacionBusiness.php';
 					require '../domain/actaAprobacion.php';
 					$acta = new actaAprobacion($idSocio,'','','progreso','');
 					$actaBusiness = new AprovacionBusiness();
 					$resultado6  = $actaBusiness->insertarActa($acta);
+
+				}
 
 				}
 
@@ -206,7 +201,6 @@
 		}
 
 		$tipoactividad = $_POST['tipoactividad'];
-		$tipofinca =  $_POST['tipofinca'];
 
 		$sociodetalle = $_POST['socioestado'];
 
@@ -215,7 +209,7 @@
 
 		$fechaingreso = $fecha[2] .'-'.$fecha[0].'-'.$fecha[1] ;
 
-		if (strlen($cedula2) &&strlen($cedula) &&strlen($nombre) &&strlen($primerapellido) &&strlen($segundoapellido) &&strlen($telmovil) &&strlen($correo) &&strlen($provincia)  &&strlen($canton) &&strlen($distrito) &&strlen($pueblo)  &&strlen($correo) &&strlen($tipoactividad)  &&strlen($tipofinca) &&strlen($fechaingreso) &&strlen($sociodetalle)  ) {
+		if (strlen($cedula2) &&strlen($cedula) &&strlen($nombre) &&strlen($primerapellido) &&strlen($segundoapellido) &&strlen($telmovil) &&strlen($correo) &&strlen($provincia)  &&strlen($canton) &&strlen($distrito) &&strlen($pueblo)  &&strlen($correo) &&strlen($tipoactividad)   &&strlen($fechaingreso) &&strlen($sociodetalle)  ) {
 
 			require 'socioBusiness.php';
 			$socioBusiness = new socioBusiness();
@@ -223,7 +217,7 @@
 		//	echo "idSocio".$socioid;
 
 				$socio = new Socio($socioid,$cedula,$nombre,$primerapellido,$segundoapellido,$telmovil,$correo,$fechaingreso,
-					$tipoactividad, $tipofinca , $sociodetalle);
+					$tipoactividad, '' , $sociodetalle);
 
 				$resultado = $socioBusiness->actualizarTBSocio($socio);
 
