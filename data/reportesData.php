200ec49034efa0424d75059777dcf2126a9e9dff
LuisCPC <lz.parrac66@gmail.com>
2017-11-06 11:52:23 -0600

    Parra

---
+++ data/reportesData.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once 'data.php';
+
+
+class reportesData extends Data{
+
+	 private $data;
+
+
+    function reportesData(){
+
+        $this->data = new Data();
+    }
+
+    public function socioCantonDistrito($canton,$distrito){
+        require '../domain/datosSocioLogin.php';
+        $socioCantonDistrito= array();
+
+        $conn = new mysqli($this->data->getServidor(), $this->data->getUsuario(), $this->data->getContrasena(), $this->data->getDbNombre());
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql="SELECT tbsocio.socioid,tbsocio.sociocedula,tbsocio.socionombre,tbsocio.socioprimerapellido,tbsocio.sociosegundoapellido,tbsocio.sociotelefono,tbsocio.sociocorreo,tbsociodireccion.socioprovincia,tbsociodireccion.sociocanton,tbsociodireccion.sociodistrito FROM tbsocio INNER JOIN tbsociodireccion ON tbsocio.socioid=tbsociodireccion.socioid AND tbsociodireccion.socioprovincia=3 AND tbsociodireccion.sociocanton='".$canton."' AND tbsociodireccion.sociodistrito='".$distrito."' ORDER BY tbsocio.socionombre ASC, tbsociodireccion.sociocanton ASC, tbsociodireccion.sociodistrito";
+        $result = $conn->query($sql);
+        if($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                array_push($socioCantonDistrito, new DatosSocioLogin($row["socioid"],$row["sociocedula"],$row["socionombre"],$row["socioprimerapellido"],$row["sociosegundoapellido"],$row["sociotelefono"]
+                    ,$row["sociocorreo"],$row["passwordlogin"]));
+            }
+        }else{
+            echo "0 results";
+        }
+        $conn->close();
+
+        return $socioCantonDistrito;
+    }
+
+
+
+    }
+}
+?>
