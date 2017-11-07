c13094ad5bbdc22107ca664b7064f0eb4d614ed2
LuisCPC <lz.parrac66@gmail.com>
2017-11-06 19:04:09 -0600

    ParraData

--- data/reportesData.php
+++ data/reportesData.php
@@ -97,7 +97,102 @@ class reportesData extends Data{
         return $datos;
     } 
     
-  
+    public function cantBovinosxDistrito(){
+        $datos= "<table border = '1'><tr><td align = 'center'>Canton - Distrito</td>  <td>Cantidad de Bovinos</td></tr>";
+        $cantTotal=0;
+        $columna= array("hatoternero","hatoternera","hatonovillo","hatonovilla","hatonovillaprenada","hatotoroservicio","hatotoroengorde","hatovacacria","hatovacaengorde");
+        $cantones= array(4,5,6); 
+        $dis4=array("Juan Viñas","Tucurrique","Pejibaye");
+        $dis5=array("Turrialba","La Suiza","Peralta","Santa Cruz","Santa Teresita","Pavones","Tuis","Tayutic","Santa Rosa","Tres Equis","La Isabel","Chirripó");
+         $dis6=array("Pacayas","Cervantes","Capellades");
+
+
+        $conn = new mysqli($this->data->getServidor(), $this->data->getUsuario(), $this->data->getContrasena(), $this->data->getDbNombre());
+        // Check connection 
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        $x=4;
+        for ($cant=0; $cant <3 ; $cant++) { //corre cantones
+
+            if($cantones[$cant]==4){//si es distrito 4
+                $cantTotal=0;
+                for ($dis=0; $dis <3 ; $dis++) { //corre distritos de canton 4
+                    for ($i=0; $i <sizeof($columna) ; $i++) {
+                        $sql="SELECT SUM($columna[$i]) AS totalDistrito  FROM tbhato INNER JOIN tbfincadireccion ON tbhato.socioid=tbfincadireccion.fincaid AND tbfincadireccion.fincaprovincia=3 AND tbfincadireccion.fincacanton=4 AND tbfincadireccion.fincadistrito=$dis+1 ORDER BY tbfincadireccion.fincacanton ASC, tbfincadireccion.fincadistrito ASC";
+                        $result = $conn->query($sql);
+                        if($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                $cantTotal+=$row["totalDistrito"];
+                            }
+
+                        }else{
+                            echo "0 results";
+                        }
+                        
+                    } 
+                 //  if($cantTotal>0){
+                    $datos=$datos."<tr><td>Jimenez - ".$dis4[$dis]." </td><td>".$cantTotal."</td></tr> ";
+                    $cantTotal=0; 
+                   //} 
+                                     
+                }
+                
+                $x=5;
+            }else if($cantones[$cant]==5){
+                $cantTotal=0;
+                for ($dist=0; $dist <12 ; $dist++) { //corre distritos de canton 4
+                    for ($i=0; $i <sizeof($columna) ; $i++) {
+                        $sql="SELECT SUM($columna[$i]) AS totalDistrito  FROM tbhato INNER JOIN tbfincadireccion ON tbhato.socioid=tbfincadireccion.fincaid AND tbfincadireccion.fincaprovincia=3 AND tbfincadireccion.fincacanton=5 AND tbfincadireccion.fincadistrito=$dist+1 ORDER BY tbfincadireccion.fincacanton ASC, tbfincadireccion.fincadistrito ASC";
+                        $result = $conn->query($sql);
+                        if($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                $cantTotal+=$row["totalDistrito"];
+                            }
+
+                        }else{
+                            echo "0 results";
+                        }
+                        
+                    } 
+                   // if($cantTotal>0){
+                        $datos=$datos."<tr><td>Turrialba - ".$dis5[$dist]." </td><td>".$cantTotal."</td></tr> ";
+                        $cantTotal=0; 
+                    //}                   
+                }
+
+                $x=6;
+            }else if($cantones[$cant]==6){
+                $cantTotal=0;
+                for ($distrito=0; $distrito <3 ; $distrito++) { //corre distritos de canton 4
+                    for ($i=0; $i <sizeof($columna) ; $i++) {
+                        $sql="SELECT SUM($columna[$i]) AS totalDistrito  FROM tbhato INNER JOIN tbfincadireccion ON tbhato.socioid=tbfincadireccion.fincaid AND tbfincadireccion.fincaprovincia=3 AND tbfincadireccion.fincacanton=6 AND tbfincadireccion.fincadistrito=$distrito+1 ORDER BY tbfincadireccion.fincacanton ASC, tbfincadireccion.fincadistrito ASC";
+                        $result = $conn->query($sql);
+                        if($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                $cantTotal+=$row["totalDistrito"];
+                            }
+
+                        }else{
+                            echo "0 results";
+                        }
+                        
+                    } 
+                    //if($cantTotal>0){
+                        $datos=$datos."<tr><td>Alvarado - ".$dis6[$distrito]." </td><td>".$cantTotal."</td></tr> ";
+                        $cantTotal=0; 
+                    //}                   
+                }
+
+
+            }
+
+            
+        }
+        $datos=$datos."</table> ";
+        $conn->close();
+        return $datos;
+    }
 
 }
 ?>
