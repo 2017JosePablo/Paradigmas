facee1761628efd2059bfe3a44203288e12b84bb
LuisCPC <lz.parrac66@gmail.com>
2017-11-12 15:20:02 -0600

    ParraData

---
+++ domain/pastoCorte.php
@@ -0,0 +1,26 @@
+<?php
+class PastoCorte{
+
+	private $id;
+	private $nombre;
+	function PastoCorte($id,$nombre){
+		$this->id=$id;
+		$this->nombre=$nombre;
+	}
+
+	public function setId($id){
+		$this->id=$id;
+	}
+	public function getId(){
+		return $this->id;
+	}
+
+	public function setNombre($nombre){
+		$this->nombre=$nombre;
+	}
+	public function getNombre(){
+		return $this->nombre;
+	}
+
+}
+?>
